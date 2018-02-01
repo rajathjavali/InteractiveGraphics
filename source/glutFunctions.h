411e3b7351cd76b319bf5b8036d0c3384b0dcf32
Rajath P Javali <rajathjavali.com>
2018-02-01 14:04:59 -0700

    Adding glew, and initial drawTriangle opengl code.

--- source/glutFunctions.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLUT_FUNCTIONS_H
-#define GLUT_FUNCTIONS_H
-
-void display();
-void idle();
-void keyboard(unsigned char key, int x, int y);
-void mouse(int button, int state, int x, int y);
-
-#endif
